afbb510f1328740f25707309dfac385e7f9b37fe
ATHI KESAVAN K <108716203+yourslovely-ak7@users.noreply.github.com>
2024-09-20 17:04:37 +0530

    Delete Task_4 directory

--- Task_4/SourceCodes/StringConst.java
+++
@@ -1,8 +0,0 @@
-package com.example.task4;
-
-public class StringConst
-{
-	public static final String NullPointerExceptionMessage= "Please, Enter a valid input!";
-	public static final String InvalidMessageLength= "The given position/index is invalid! Kindly recheck.";
-	public static final String InvalidMessageZero= "The position/index should be greater than zero! Kindly recheck.";
-}
