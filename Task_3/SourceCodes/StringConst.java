8590514edecfb6d56e80ee6f9c37a201fcc7e821
ATHI KESAVAN K <108716203+yourslovely-ak7@users.noreply.github.com>
2024-09-20 17:04:28 +0530

    Delete Task_3 directory

--- Task_3/SourceCodes/StringConst.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-public class StringConst
-{
-	public static final String NullPointerExceptionMessage= "Please, Enter a valid input!";
-	public static final String InvalidMessageLength= "The required length value is higher than the String length! Kindly recheck.";
-	public static final String InvalidMessageZero= "The required length should be greater than zero! Kindly recheck.";
-}
